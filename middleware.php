9b171572326c9f3a9af501585df5dbbb9023b9d1
MortallicaXxX <guillaume.houthoofd@gmail.com>
2022-06-08 20:21:40 +0200

    init Rapido@DotEnv middleware

--- middleware.php
+++ middleware.php
@@ -7,34 +7,21 @@
   *Use-case :
   *Sample :
 */
-class Middleware{
-  /**
-    *Description :
-  */
-  protected $type = "middleware";
-  /**
-    *Description :
-  */
-  public function get_type(){return $this -> type;}
-  /**
-    *Description :
-  */
-  public function Program($routeur){return $routeur;}
-}
-
-/**
-  *Name : Router
-  *Type : Class
-  *Description :
-  *Use-case :
-  *Sample :
-*/
-class Error{
-
-  function __construct($msg){
-    var_dump($msg);
+if(!class_exists('Middleware')){
+  class Middleware{
+    /**
+      *Description :
+    */
+    protected $type = "middleware";
+    /**
+      *Description :
+    */
+    public function get_type(){return $this -> type;}
+    /**
+      *Description :
+    */
+    public function Program($routeur){return $routeur;}
   }
-
 }
 
 ?>
