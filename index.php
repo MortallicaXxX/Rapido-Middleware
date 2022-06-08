bd62fc3be0995f98dadda1936e04a3d63e22a048
MortallicaXxX <guillaume.houthoofd@gmail.com>
2022-06-08 16:41:14 +0200

    init Rapido@DotEnv middleware

--- index.php
+++ index.php
@@ -1,16 +1,44 @@
 <?php
 include_once("middleware.php");
 
-class MyMiddleware extends Middleware{
 
-  function __construct($args){
+  class DotEnv extends Middleware{
+
+    private $filePath;
+
+    function __construct($filePath){
+      $this -> filePath = $filePath;
+    }
+
+    function Program($router){
+      if($this -> __is_file_exist() == true){
+        $router["dotenv"] = $this -> __load_file();
+      }
+      return $router;
+    }
+
+    private function __is_file_exist(){
+      return is_file($this -> filePath);
+    }
+
+    private function __load_file(){
+      return $this -> __normalize((new \Tools\FileSystem()) -> read_file($this -> filePath));
+    }
+
+    private function __normalize($strFile){
+      $lines = array_filter(explode("\n",$strFile), function($item){return $item;});
+      $result = array();
+      for($i = 0 ; $i < count($lines) ; $i++){
+        $keyLine = array_keys($lines)[$i];
+        $lines[$keyLine] = explode("=",$lines[$keyLine]);
+        for($y = 0; $y < count($lines[$keyLine]); $y++) {
+          $keyCell = array_keys($lines[$keyLine])[$y];
+          $lines[$keyLine][$keyCell] = trim($lines[$keyLine][$keyCell]);
+        }
+        $result[$lines[$keyLine][0]] = $lines[$keyLine][1];
+      }
+      return $result;
+    }
 
   }
-
-  public function Program($routeur){
-    // TOTO :
-    return $router;
-  }
-
-}
 ?>
